f62d2f18259f06c9416663b2d6646313c3889350
Marcos <exegeses@gmail.com>
2023-07-11 12:12:55 -0300

    creación de Model y Controller para la tabla marcas

--- agencia/resources/views/inicio.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.plantilla')
-
-    @section('contenido')
-        contenido
-
-    @endsection
--- agencia/resources/views/resultado.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.plantilla')
-
-    @section('contenido')
-
-        Tu nombre es: {{ $nombre }}
-
-    @endsection
